f7b1bc33362216b20605b94f4e1fe58dbe0e6926
rajeevranjanpan <54030044+rajeevranjanpan@users.noreply.github.com>
2022-02-17 13:56:49 +0530

    Add files via upload

---
+++ Competitive_Coding_Java-master/src/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+
+public class Node {
+
+    int data;
+    public Node left;
+    public Node right;
+   public Node next;
+    Node prev;
+Node random;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
